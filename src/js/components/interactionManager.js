c4e6c4203e3ce09639b852376385b23d3f3a0f29
Mg Htoo <whshwe@gmail.com>
2025-05-19 12:25:05 +0630

    add/remove highlight on the TTTBoard squares

--- src/js/components/interactionManager.js
+++ src/js/components/interactionManager.js
@@ -1,9 +1,14 @@
 import { selectors } from "../services/selectors.js";
 import { INTERACTIONS } from "../constants/appConstants.js";
+import { CSS_CLASS_NAMES } from "../constants/cssClassNames.js";
 
 export function interactionManager() {
-  function _hightlightSquare() {
-    
+  function _addHighlight(targetElement) {
+    targetElement.classList.add(CSS_CLASS_NAMES.HIGHLIGHT);
+  }
+
+  function _removeHighlight(targetElement) {
+    targetElement.classList.remove(CSS_CLASS_NAMES.HIGHLIGHT);
   }
 
   function _addSquareListeners() {
@@ -12,6 +17,13 @@ export function interactionManager() {
 
     gameBoard.addEventListener("mouseover", (event) => {
       if(event.target.matches(matchingID)) {
+        _addHighlight(event.target);
+      }
+    });
+
+    gameBoard.addEventListener("mouseout", (event) => {
+      if(event.target.matches(matchingID)) {
+        _removeHighlight(event.target);
       }
     });
 

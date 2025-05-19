1c9f0984aa7ad6d19ab6423695bc6d0b32420893
Mg Htoo <whshwe@gmail.com>
2025-05-19 11:27:34 +0630

    loading spinner added

---
+++ src/js/components/loadingManager.js
@@ -0,0 +1,38 @@
+import { selectors } from "../services/selectors.js";
+
+export function loadingManager(initializeInput) {
+  function _showLoadingSpinner() {
+    selectors.overlay.classList.remove("invisible");
+    selectors.loadingWrapper.classList.remove("invisible");
+  }
+
+  function _hideLoadingSpinner() {
+    selectors.overlay.classList.add("invisible");
+    selectors.loadingWrapper.classList.add("invisible");
+  }
+
+  function _asyncWrapper(fn) {
+    return async (...args) => {
+      try {
+        await fn(...args);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  }
+
+
+  async function start(){
+    const _initializeInput = _asyncWrapper(initializeInput);
+
+    _showLoadingSpinner();
+    await _initializeInput();
+    //await new Promise(resolve => setTimeout(resolve, 1000)); // Add a 1-second delay (use this if necessary)
+    _hideLoadingSpinner();
+  }
+
+  return {
+    start,
+  };
+
+}

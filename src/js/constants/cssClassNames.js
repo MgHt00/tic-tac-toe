e069b01d310feaf1f79b80fa8bcd5aed2b20a472
Mg Htoo <whshwe@gmail.com>
2025-05-24 10:56:04 +0630

    interaction: changed winningsqure-class names

--- src/js/constants/cssClassNames.js
+++ src/js/constants/cssClassNames.js
@@ -1,4 +1,9 @@
 export const CSS_CLASS_NAMES = {
   INVISIBLE : "invisible",
   HIGHLIGHT : "highlight",
+
+  WIN_ROW: "winning-square-row", // Row
+  WIN_COLUMN: "winning-square-column", // Column
+  WIN_DIAGONAL_MAIN: "winning-square-diagonal-main", // Top-left to bottom-right (e.g., \)
+  WIN_DIAGONAL_ANTI: "winning-square-diagonal-anti", // Top-right to bottom-left (e.g., /)
 }

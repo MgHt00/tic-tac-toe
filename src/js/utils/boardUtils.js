4e30efd8f5a2ac27cb2fc1e3d5cde3b46b199b11
Mg Htoo <whshwe@gmail.com>
2025-06-24 10:31:50 +0630

    INTERACTION: Gemini's winning condition check fns approved; X _ X X to X X X X bug fixed

--- src/js/utils/boardUtils.js
+++ src/js/utils/boardUtils.js
@@ -30,20 +30,20 @@ export function getEmptySquares(gameBoard) {
   return emptySquares;
 }
 
-// Defines all possible winning combinations on a 3x3 Tic-Tac-Toe board.
-const _winningCombinationsByBoard = {
-  row1: [0, 1, 2],
-  row2: [3, 4, 5],
-  row3: [6, 7, 8],
-  col1: [0, 3, 6],
-  col2: [1, 4, 7],
-  col3: [2, 5, 8],
-  diag1: [0, 4, 8],
-  diag2: [2, 4, 6],
-};
-
 /* Checks if the current player has won the game. */
 export function checkWinCondition(gameBoard, currentPlayer) {
+  // Defines all possible winning combinations on a 3x3 Tic-Tac-Toe board.
+  const _winningCombinationsByBoard = {
+    row1: [0, 1, 2],
+    row2: [3, 4, 5],
+    row3: [6, 7, 8],
+    col1: [0, 3, 6],
+    col2: [1, 4, 7],
+    col3: [2, 5, 8],
+    diag1: [0, 4, 8],
+    diag2: [2, 4, 6],
+  };
+
   const _flatGameBoard = gameBoard.flat();
   for (const key in _winningCombinationsByBoard) {
     const indices = _winningCombinationsByBoard[key];
